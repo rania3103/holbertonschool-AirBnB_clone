5494e1d595bdcc19f9ccd6d4c4a0429694289d39
rania3103 <raniaabassi33@gmail.com>
2023-11-05 12:55:52 +0100

    fix __str__

--- models/base_model.py
+++ models/base_model.py
@@ -10,16 +10,16 @@ from datetime import datetime
 class BaseModel:
     """this is the class BaseModel"""
 
-    def __init__(self, id=uuid.uuid4()):
+    def __init__(self):
         """constructor"""
-        self.id = str(id)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """return string representation"""
-        return "{} {} {}".format(
-            [self.__class__.__name__], (self.id), self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
