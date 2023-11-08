3bbf1201574171e48845af04b70c434152ea07cf
rania3103 <raniaabassi33@gmail.com>
2023-11-08 18:44:07 +0100

    fix pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""Write a class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances"""
+"""Write a class FileStorage that serializes
+instances to a JSON file and deserializes
+JSON file to instances"""
 import json
 from models.base_model import BaseModel
+
 
 class FileStorage:
     """this is the class FileStorage"""
@@ -13,12 +16,14 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """sets in __objects the obj with key
+        <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """serializes __objects to the JSON file
+        (path: __file_path)"""
         with open(self.__file_path, 'w') as f:
             dict = {}
             for k, v in self.__objects.items():
@@ -26,7 +31,10 @@ class FileStorage:
             json.dump(dict, f)
 
     def reload(self):
-        """ deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
+        """ deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists
+        otherwise, do nothing. If the file doesn’t exist
+        no exception should be raised)"""
         try:
             with open(self.__file_path, 'r') as f:
                 dict = json.load(f)
