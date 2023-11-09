1c1d39fb6bcba0ccf7efb7c2159e539397c31b0b
rania3103 <raniaabassi33@gmail.com>
2023-11-09 13:48:54 +0100

    import new classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,12 @@ instances to a JSON file and deserializes
 JSON file to instances"""
 import json
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 class FileStorage:
     """this is the class FileStorage"""
