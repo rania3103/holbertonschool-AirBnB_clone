c5a41e364910abb2d76f7e4c575f0b6529ba6c90
rania3103 <raniaabassi33@gmail.com>
2023-11-09 13:55:56 +0100

    import new classes

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
