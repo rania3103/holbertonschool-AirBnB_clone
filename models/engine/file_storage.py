082ee0bfcc3aee7468fdd02e97fd91bb57d14f13
rania3103 <raniaabassi33@gmail.com>
2023-11-09 14:39:04 +0100

    import BaseModel

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 instances to a JSON file and deserializes
 JSON file to instances"""
 import json
+from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
 from models.state import State
